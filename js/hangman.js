8cd9557a87eb63ae04ea8b01d947becba022bd32
johny11111 <125285264+johny11111@users.noreply.github.com>
2023-05-12 01:00:07 +0300

    hangman checkwin

--- js/hangman.js
+++ js/hangman.js
@@ -1,38 +1,29 @@
 let guesses = "";
-let board = ["-","-","-","-","-"]
+let board = ["-", "-", "-", "-", "-"]
 const letters_selected = [
-    ["apple"],
-    ["lemon"],
-    ["hello"]
+    "apple",
+    "lemon",
+    "hello"
 ]
 
-function checkWin(){
-    letters_selected.forEach((letter) =>{
-        for(let i = 0; i < letter.length ;i++){
-            let guseT = guesses.trim()
-            let letterCopy = letter.filter(letter => letter.includes(guseT))
-         
-            if(letterCopy){
-                for(j= 0; j < letterCopy.length; j++){
-                    debugger
-                    document.querySelector("#showSelectLeter").innerHTML = ""
-                    console.log(letterCopy[j]);
-                    board[i] = letterCopy[i]
-                    console.log(board);
-                    document.querySelector("#showSelectLeter").innerHTML = createSign(board)
-                }
-
-             
+let random = Math.floor(Math.random()*3)
+function checkWin(e) {
+    let whoClickedValue = e.target.textContent;
+        for (let i = 0; i < letters_selected[random].length; i++) {
+            if (letters_selected[random][i].includes(whoClickedValue)){
+                console.log("dh");
+                board[i] = letters_selected[random][i];
+                document.querySelector("#showSelectLeter").innerHTML = board     
             }
-        }  
-    })
+        }
+    }
 
 
-  
-}
+
+
 
 // arry for results
-function createSign(arr_letter){
+function createSign(arr_letter) {
     arr_letter.forEach((e) =>
         document.querySelector("#showSelectLeter").innerHTML += e);
 }
@@ -45,13 +36,14 @@ function createLetter() {
     const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     let divElement;
     let div;
-    for (let i = 0; i <= 25; i++){
+    for (let i = 0; i <= 25; i++) {
         div = document.createElement('div');
         div.className = "squer"
         div.innerHTML = letters[i];
         div.id = `div${i}`;
         div.addEventListener("click", (e) => {
             onClick(e)
+            checkWin(e)
         })
         divElement = document.querySelector("#lateer").append(div)
     }
@@ -65,13 +57,12 @@ function onClick(e) {
     let whoClickedValue = e.target.innerHTML;
     let whoClickedID = e.target.id;
     addSelect(whoClickedValue)
-    checkWin()
-   
+
 }
 
 
 // add selected to guesses
 function addSelect(arr) {
     guesses += arr + " "
-    document.querySelector('#guues').innerHTML += guesses
+    document.querySelector('#guues').innerHTML = guesses
 }
